fd000b1ea973ffd31712cc2fdf045c0355290bdb
Ali <yamen.ali@sap.com>
2020-02-03 16:26:43 +0100

    add generator docs

--- src/m4_generator.py
+++ src/m4_generator.py
@@ -7,6 +7,22 @@ from tensorflow.keras import backend as K
 
 class M4Generator(Sequence):
 
+    """ 
+        This class implements a data generator that will return data in batches
+
+        Args:
+            X (array_like): The insample data ( i.e. The timeseries fed to the model as input)
+            Y (array_like): The target data (i.e. The expected timesereis at the output)
+            batch_size (int): The number of samples in one batch
+            features_number (int): The number of features in input data
+
+        Attributes:
+            X (array_like): The insample data ( i.e. The timeseries fed to the model as input)
+            Y (array_like): The target data (i.e. The expected timesereis at the output)
+            batch_size (int): The number of samples in one batch
+            features_number (int): The number of features in input data
+    """
+
     def __init__(self, X, Y, batch_size, features_number):
         
         self.batch_size = batch_size
@@ -15,9 +31,26 @@ class M4Generator(Sequence):
         self.features_number = features_number
 
     def __len__(self):
+        """
+        Get the total number of batches
+
+        Returns:
+            int: The total number of batches
+
+        """
         return int(np.floor(len(self.X) / float(self.batch_size)))
 
     def __getitem__(self, idx):
+        """
+        Get a pair of items (input, target) based on their index in the data array
+
+        Args:
+            idx (int): The item index in the data array
+
+        Returns:
+            (array_like, array_like): (input timeserie, target timeserie)
+
+        """
         batch_x = self.X[idx * self.batch_size:(idx + 1) * self.batch_size, :]
         batch_y = self.Y[idx * self.batch_size:(idx + 1) * self.batch_size, :]
 
@@ -26,4 +59,11 @@ class M4Generator(Sequence):
         return batch_x, batch_y
 
     def steps_per_epoch(self):
+        """
+        Get the number of steps in each epochs (i.e. the total number of batches)
+
+        Returns:
+            int: The numebr of steps in each epochs
+
+        """
         return self.__len__()
