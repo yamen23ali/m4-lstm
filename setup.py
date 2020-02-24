ebcc9fce71928625c27971ddbdad647600889170
Ali <yamen.ali@sap.com>
2020-02-24 13:39:54 +0100

    setup docs

--- setup.py
+++
@@ -1,8 +0,0 @@
-# setup.py
-from setuptools import setup, find_packages
-setup(
-    name = "lstm",
-    version = "0.0.1",
-    packages = find_packages(),
-    data_files=[('.', ['__main__.py', '__init__.py'])]
-)
